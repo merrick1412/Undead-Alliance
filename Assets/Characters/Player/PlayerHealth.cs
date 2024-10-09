aab0ab06b7777740a3820158d8855c062a92b432
Jacob Cabrera <jacobcabrera718@gmail.com>
2024-10-09 12:31:46 -0400

    Zombie Free Asset
    Add zombie asset and playerhealth.cs that aligns with damage delt by zombie.

---
+++ Assets/Characters/Player/PlayerHealth.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player has died!");
+        // Handle player death (respawn, game over, etc.)
+    }
+}
