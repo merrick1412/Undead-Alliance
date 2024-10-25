68dab3ac925977a9185f0c977c3f835d0bcaaa74
Isaac <isaacmcd03@gmail.com>
2024-10-25 00:23:50 -0400

    UI work for leveling system + leveling system progress
    there is a bug for the 500 button where the bar doesn't update properly, will fix later.

--- Assets/Leveling System/LevelSystem.cs
+++ Assets/Leveling System/LevelSystem.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelSystem { 
-    
+public class LevelSystem {
+
+    public event EventHandler OnExperienceChanged;
+    public event EventHandler OnLevelChanged;
+
     private int level;
     private int experience;
     private int experienceToNextLevel;
@@ -20,10 +24,16 @@ public class LevelSystem {
             // Enough experience to level up
             level++;
             experience -= experienceToNextLevel;
+            if (OnLevelChanged != null) OnLevelChanged(this, EventArgs.Empty);
         }
+        if (OnExperienceChanged != null) OnExperienceChanged(this, EventArgs.Empty);
     }
 
     public int GetLevelNumber() {
         return level;
+    }
+
+    public float GetExperienceNormalized() {
+        return (float)experience / experienceToNextLevel;
     }
 }
--- Assets/Leveling System/Testing.cs
+++ Assets/Leveling System/Testing.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour {
 
+    [SerializeField] private LevelWindow levelWindow;
+
     private void Awake() {
         LevelSystem levelsystem = new LevelSystem();
-        Debug.Log(levelsystem.GetLevelNumber());
-        levelsystem.AddExperience(50);
-        Debug.Log(levelsystem.GetLevelNumber());
-        levelsystem.AddExperience(60);
-        Debug.Log(levelsystem.GetLevelNumber());
+        levelWindow.SetLevelSystem(levelsystem);
     }
 }
