630f09b4fc7bc1ed6c04a46ddd9c42f1ccf11ab4
merrick1412 <mwmoncure@gmail.com>
2024-11-12 22:13:38 -0500

    work on dropped weapons
    more work on dropping weapons and picking them back up

--- Assets/Scripts/Inventory/PlayerInventoryController.cs
+++ Assets/Scripts/Inventory/PlayerInventoryController.cs
@@ -1,13 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerInventoryController : MonoBehaviour
 {
     private Inventory inventory;
+    public bool WeaponNearby;
+    public Weapon convertedWeapon;
+    private DroppedWeapon nearbyDroppedWeapon;
+    GameObject newWeapon;
     void Start()
     {
         inventory = GetComponent<Inventory>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DroppedWeapon droppedWeapon = collision.GetComponent<DroppedWeapon>(); //if touching a dropped weapon
+        if (droppedWeapon != null)
+        {
+            nearbyDroppedWeapon = droppedWeapon;
+            ConvertNearbyWeapon(nearbyDroppedWeapon); //player is near a weapon, converts dropped weapon into a weapon
+            WeaponNearby = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        DroppedWeapon droppedWeapon = collision.GetComponent<DroppedWeapon>();
+        if (droppedWeapon != null && droppedWeapon == nearbyDroppedWeapon)
+        {
+            nearbyDroppedWeapon = null; //player moved away from weapon
+        }
+    }
+
+    public GameObject ConvertNearbyWeapon(DroppedWeapon drop)
+    {
+       
+        newWeapon.AddComponent<Weapon>();
+        newWeapon.AddComponent<SpriteRenderer>(); //creates game object with the correct scripts
+        newWeapon.GetComponent<Weapon>().CopyStats(drop.GetComponent<Weapon>()); //copies the stats
+        return Instantiate(newWeapon,inventory.GetCurrentWeapon().transform.position,Quaternion.identity);
     }
 
     // Update is called once per frame
