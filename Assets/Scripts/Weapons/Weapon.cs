d7d241b398a8752c80a0ba0d5f0610369e76053a
merrick1412 <120765132+merrick1412@users.noreply.github.com>
2024-11-24 22:44:35 -0500

    More work on inventory
    forgot to hit save before my last push

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -13,44 +13,20 @@ public class Weapon : MonoBehaviour //parent class for weapons
     public float rateOfFire;
     public bool Automatic;
     public int MagazineSize;
-    public Inventory inventory;
+    
     public PlayerInventoryController playerInventoryController;
 
     void Start()
     {
-        Initialize();
-    }
-
-    void Initialize()
-    {
-        // Try to get the Inventory component from the parent
         
-        if (inventory == null)
-        {
-            Debug.LogError("Can't get Inventory component");
-        }
     }
 
-    void OnEnable()
-    {
-        // Initialize when the weapon is enabled
-        Initialize();
-    }
+    
 
     void Update()
     {
-        // Make sure inventory is not null before accessing it
-        if (inventory == null)
-        {
-            Debug.LogWarning("Inventory is null in Update, attempting to reinitialize");
-            Initialize();
-        }
-
-        if (inventory != null && inventory.currentWeapon == this)
-        {
-            Debug.Log($"Current weapon is {GetComponentInParent<Inventory>().currentWeapon}");
-            gameObject.SetActive(true);
-        }
+       
+      
     }
 
     public bool isAutomatic()
@@ -58,17 +34,8 @@ public class Weapon : MonoBehaviour //parent class for weapons
         return Automatic;
     }
 
-    public void HideWeapon()
-    {
-        gameObject.SetActive(false);
-        Debug.Log($"Deactivating {this.name}");
-    }
-
-    public void ShowWeapon()
-    {
-        gameObject.SetActive(true);
-        Debug.Log($"Activating {this.name}");
-    }
+    
+    
 
     public void CopyStats(Weapon other)
     {
@@ -81,7 +48,7 @@ public class Weapon : MonoBehaviour //parent class for weapons
         MagazineSize = other.MagazineSize;
         bulletPrefab = other.bulletPrefab;
         ammoType = other.ammoType;
-        inventory= other.inventory;
+        
     }
 
 
