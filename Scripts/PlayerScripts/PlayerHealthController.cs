d414b2dd7b1a4a353403565428c25e42ddca9f12
merrick1412 <120765132+merrick1412@users.noreply.github.com>
2024-12-13 19:36:22 -0500

    Revert "main menu upon death"
    This reverts commit aadcbf19362a15170aee9e3f565aee53b719c4fe.

--- Scripts/PlayerScripts/PlayerHealthController.cs
+++ Scripts/PlayerScripts/PlayerHealthController.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
 
-public class PlayerHealthController : MonoBehaviour 
+public class PlayerHealthController : MonoBehaviour
 {
     [SerializeField] //this allows health to be edited in unity editor
-    public float maxPlHealth;
+    private float maxPlHealth;
     [SerializeField]
-    public float currentPlHealth;
+    private float currentPlHealth;
+
     
-    [SerializeField]
-    private string mainMenuSceneName = "MainMenu"; // Add this to set the main menu scene name in Unity Editor
-    
+
     public float healthPercent
     {
         get
@@ -21,12 +19,10 @@ public class PlayerHealthController : MonoBehaviour
             return currentPlHealth / maxPlHealth;//grabs a percent of remaining hp
         }
     }
-    
     public bool IFrame { get; set; }
-    
+
     public UnityEvent OnDamaged;
-    public UnityEvent OnDeath; // Added event for death
-    
+
     public void TakeDamage(float damageAmnt)
     {
         if(currentPlHealth == 0) { //if health is already 0 dont do anything
@@ -36,31 +32,17 @@ public class PlayerHealthController : MonoBehaviour
         {
             return;
         }
-        
+
         currentPlHealth -= damageAmnt;
-        if (currentPlHealth <= 0)
+        if (currentPlHealth < 0)
         {
             currentPlHealth = 0; //if hp is negative fixes
-            Die(); // Call the die method when health reaches 0
         }
         else
         {
             OnDamaged.Invoke(); //if taken damage without dying, IFrame activated
         }
     }
-    
-    private void Die()
-    {
-        OnDeath.Invoke(); // Trigger death event
-        StartCoroutine(LoadMainMenuWithDelay());
-    }
-    
-    private IEnumerator LoadMainMenuWithDelay()
-    {
-        yield return new WaitForSeconds(2f); // Add a small delay before loading main menu
-        SceneManager.LoadScene(mainMenuSceneName);
-    }
-    
     public void AddHealth(float amountToAdd)
     {
         if (currentPlHealth == maxPlHealth) { //if health is already maxed
@@ -70,15 +52,5 @@ public class PlayerHealthController : MonoBehaviour
         if (currentPlHealth > maxPlHealth) { //adds health and accounts for overflow
             currentPlHealth = maxPlHealth;
         }
-    }
-    
-    public bool IsAlive()
-    {
-        return currentPlHealth > 0;
-    }
-    
-    public void setMaxHealth(float amountToAdd)
-    {
-        maxPlHealth = maxPlHealth + amountToAdd;
     }
 }
