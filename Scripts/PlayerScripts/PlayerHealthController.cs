ae342b9b18b87cd7da838c19ce3d8da8ac8781f1
dillonMann21 <dillonmann21@gmail.com>
2024-12-13 20:06:26 -0500

    Update PlayerHealthController.cs

--- Scripts/PlayerScripts/PlayerHealthController.cs
+++ Scripts/PlayerScripts/PlayerHealthController.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
-public class PlayerHealthController : MonoBehaviour
+public class PlayerHealthController : MonoBehaviour 
 {
     [SerializeField] //this allows health to be edited in unity editor
-    private float maxPlHealth;
+    public float maxPlHealth;
     [SerializeField]
-    private float currentPlHealth;
-
+    public float currentPlHealth;
     
-
     public float healthPercent
     {
         get
@@ -19,10 +18,11 @@ public class PlayerHealthController : MonoBehaviour
             return currentPlHealth / maxPlHealth;//grabs a percent of remaining hp
         }
     }
+    
     public bool IFrame { get; set; }
-
+    
     public UnityEvent OnDamaged;
-
+    
     public void TakeDamage(float damageAmnt)
     {
         if(currentPlHealth == 0) { //if health is already 0 dont do anything
@@ -32,17 +32,19 @@ public class PlayerHealthController : MonoBehaviour
         {
             return;
         }
-
+        
         currentPlHealth -= damageAmnt;
-        if (currentPlHealth < 0)
+        if (currentPlHealth <= 0)
         {
             currentPlHealth = 0; //if hp is negative fixes
+            SceneManager.LoadScene("Main Menu"); // Load main menu when health reaches zero
         }
         else
         {
             OnDamaged.Invoke(); //if taken damage without dying, IFrame activated
         }
     }
+    
     public void AddHealth(float amountToAdd)
     {
         if (currentPlHealth == maxPlHealth) { //if health is already maxed
@@ -52,5 +54,10 @@ public class PlayerHealthController : MonoBehaviour
         if (currentPlHealth > maxPlHealth) { //adds health and accounts for overflow
             currentPlHealth = maxPlHealth;
         }
+    }
+    
+    public void setMaxHealth(float amountToAdd)
+    {
+        maxPlHealth = maxPlHealth + amountToAdd;
     }
 }
